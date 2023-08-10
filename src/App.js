60e890991b5492ca1cebbf12da6108d5386ce9d2
Ajazkhan10 <99741323+Ajazkhan10@users.noreply.github.com>
2023-08-10 12:44:49 +0500

    fix:responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import HeroSection from "./Components/Herosection";
+// import HeroSection from "./Components/Herosection";
+import Footer from "./Components/Footer/Footer";
+import HeroSection from "./Components/HeroSection/HeroSection";
+import IncrementDecrement from "./Components/IncrementDecrement/IncrementDecrement";
+import MainSection from "./Components/MainSection/MainSection";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <HeroSection />
+    <>
+      <Navbar />
+      <HeroSection />
+      <MainSection />
+      <IncrementDecrement />
+      <Footer />
+      {/* <HeroSection /> */}
+    </>
   );
 }
 
