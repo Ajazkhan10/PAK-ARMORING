73e7a7e672542e44b2fec5f671406881d99d5b23
Ajazkhan10 <rafiqkhan1343@gamil.com>
2023-08-15 17:22:33 +0500

    feat: complete responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../Assert/svg/image.svg";
+// import logo from "../../Assert/svg/image.svg";
 // import facebook from "../../Assert/svg/facebook.svg";
 // import twitter from "../../Assert/svg/twitter.svg";
 // import linkedin from "../../Assert/svg/akar-icons_linkedin-fill.svg";
@@ -7,23 +7,16 @@ import logo from "../../Assert/svg/image.svg";
 const Footer = () => {
   return (
     <div>
-      <section className="footer-main flex flex-col items-start gap-[40px] lg:flex-row  lg:gap-[120px]  bg-[#000] text-[#fff]  py-[80px] w-full">
-        <div className="footer_box   items-start">
-          <div className="logoSide flex gap-[20px] px-[80px] ">
-            <div className="logo_img">
-              <img src={logo} alt="logo" className="w-[62px] h-[70px] " />
-            </div>
-            <div className="text-[#fff] font-Ubuntu not-italic leading-normal ">
-              <h2 className="text-[20px] font-bold">Pak Armoring (PVT) LTD.</h2>
-              <h4 className="text-[12px] font-medium">
-                Armored Vehicles Specialist
-              </h4>
-            </div>
-          </div>
+      <section className=" bg-secondary_gray_300 text-light_white grid  lg:grid-cols-4 py-[30px] px-[30px]">
+        <div className="flex flex-col items-start">
+          <h2 className="text-[20px] font-bold">Pak Armoring (PVT) LTD.</h2>
+          <h4 className="text-[12px] font-medium">
+            Armored Vehicles Specialist
+          </h4>
         </div>
-        <div class="footer_box flex flex-col gap-[30px] px-[80px]">
-          <h2 className="text-[20px] font-bold">About Company</h2>
-          <ul className="flex flex-col gap-[20px] px-[30px]">
+        <div>
+          <h2 className="text-[20px] font-bold pb-[30px]">About Company</h2>
+          <ul className="flex flex-col gap-[10px] ">
             <li>
               <a href="aDd">Overview</a>
             </li>
@@ -44,52 +37,33 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div class="footer_box flex flex-col  gap-[30px] px-[80px]">
-          <h2 className="text-[20px] font-bold ">Products</h2>
-          <ul className="grid grid-rows-6 grid-flow-col gap-[20px] gap-x-[50px]  px-[30px]">
+        <div>
+          <h2 className="text-[20px] font-bold pb-[30px] ">Overview</h2>
+          <ul className="flex flex-col gap-[10px] ">
             <li>
-              <a href="vips_inner?link=BMW">BMW X Series</a>
+              <a href="about-us">Company Profile</a>
             </li>
             <li>
-              <a href="vips_inner?link=Range_Rover">Range Rover</a>
+              <a href="ceo-message">CEO Message</a>
             </li>
             <li>
-              <a href="vips_inner?link=Toyota_Fortuner">Fortuner</a>
+              <a href="vision-and-mission">Mission &amp; Vision</a>
             </li>
             <li>
-              <a href="vips_inner?link=Kia_Sorento">Sorento</a>
+              <a href="asfasd">Certifications</a>
             </li>
             <li>
-              <a href="vips_inner?link=Mercedes_Benz">Mercedez Benz</a>
-            </li>
-
-            <li>
-              <a href="vips_inner?link=Toyota_Corolla">Toyota Corolla</a>
-            </li>
-            <li>
-              <a href="vips_inner?link=Toyota_Camry">Toyota Camry</a>
-            </li>
-            <li>
-              <a href="vips_inner?link=Toyota_Vigo">Toyota Revo</a>
-            </li>
-            <li>
-              <a href="vips_inner?link=HiAce">HiAc</a>
-            </li>
-            <li>
-              <a href="vips_inner?link=Coaster">Coaster</a>
-            </li>
-            <li>
-              <a href="MultiPurposeVehicles">Sagr APC &amp; UQAAB</a>
+              <a href="fassfsa">Testimonials</a>
             </li>
           </ul>
         </div>
-        <div class="footer_box flex flex-col px-[80px]">
-          <h2 className="text-[20px] font-bold py-[30px] ">Office Address:</h2>
-          <p className="px-[30px]">
+        <div>
+          <h2 className="text-[20px] font-bold pb-[30px] ">Office Address:</h2>
+          <p className="">
             Plot No. 2/2a, Survey 86, Sector 24, Main Korangi Creek Road,
             Karachi.
           </p>
-          <ul class="address px-[30px]">
+          <ul class="address gap-[10px] ">
             <li class="flex_">
               <span>
                 <i class="fas fa-phone-volume"></i>
